4291ddf73bd2f9acdd092693a98d7a1b925b100e
Ryan Maloney <rypmaloney@gmail.com>
2022-02-10 09:03:06 -0500

    updated with express for /

--- scripts.js
+++ scripts.js
@@ -1,46 +1,61 @@
 const http = require("http")
 const fs = require("fs")
 
-const server = http.createServer((req,res)=>{
-    console.log(req.url)
+const port = 8080;
 
-    res.setHeader("Content-Type", "text/html")
-    let path = "./routes/"
-    switch(req.url){
-        case "/":
-            path += "index.html";
-            break;
-        case "/about":
-            path += "about.html";
-            break;
-        case "/contact":
-            path += "contact.html";
-            break;
-        default :
-            path+="404.html";
-            break;
-    }
+const express = require('express')
+const app = express()
 
-    fs.readFile(path, (err, data) =>{
-        if (err){
-            console.log(err)
-        } else {
-            res.write(data)
 
-        }
-    })
-    res.setHeader("Content-Type", "text/html")
-    fs.readFile("./style.css", (err, data) =>{
-        if (err){
-            console.log(err)
-        } else {
-            res.write(data)
-            res.end()
-        }
-    })
+app.get('/', function(req, res) {
+    res.send('./routes/index.html')
+  });
 
+app.listen(port, ()=>{
+    console.log("App is listening on port 8080")
 })
 
-server.listen(8080, 'localhost', () =>{
-    console.log("The server is listening on localhost port 8080")
-})
+
+// const server = http.createServer((req,res)=>{
+//     console.log(req.url)
+
+//     res.setHeader("Content-Type", "text/html")
+//     let path = "./routes/"
+//     switch(req.url){
+//         case "/":
+//             path += "index.html";
+//             break;
+//         case "/about":
+//             path += "about.html";
+//             break;
+//         case "/contact":
+//             path += "contact.html";
+//             break;
+//         default :
+//             path+="404.html";
+//             break;
+//     }
+
+//     fs.readFile(path, (err, data) =>{
+//         if (err){
+//             console.log(err)
+//         } else {
+//             res.write(data)
+
+//         }
+//     })
+//     res.setHeader("Content-Type", "text/html")
+//     fs.readFile("./style.css", (err, data) =>{
+//         if (err){
+//             console.log(err)
+//         } else {
+//             res.write(data)
+//             res.end()
+//         }
+//     })
+
+// })
+
+// server.listen(8080, 'localhost', () =>{
+//     console.log("The server is listening on localhost port 8080")
+// })
